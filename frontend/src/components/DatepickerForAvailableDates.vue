bb05301a35887280bb2a941fb2700cf275be9975
Mikael Rusin <mikael.rusin@gmail.com>
2021-05-03 18:42:09 +0200

    Laid base in terms of Datepicker for availability

--- frontend/src/components/DatepickerForAvailableDates.vue
+++ frontend/src/components/DatepickerForAvailableDates.vue
@@ -1,24 +1,29 @@
+<script setup>
+import Datepicker from 'vue3-datepicker'
+import { ref } from 'vue'
+import { add } from 'date-fns'
+//NOTE: Datepickers are Readonly in properties, meaning you cannot directly attach CSS to them outside of built in
+// API - which only affects the input field in terms of selection of Date
+let disabledDate = ref([new Date("2021-05-08"), new Date("2021-05-09")]) //Just add dates here to have them be disabled
+//Look into making it able to parse in loop in terms of Datepicker dates
+let thisDay = ref(new Date())
+</script>
+
 <template>
-  <div class="fromDatePickerDiv">
-      <div><label for="fromDatePicker">From the:</label>
-      <datepicker id="fromDatePicker" class="fromDatePicker" v-model="startDate" :lower-limit="minimumDate" :upper-limit="endDate" :disabledDates="takenDates"/></div>
-  </div>
-  <div class="toDatePickerDiv">
-    <label for="toDatePicker">To the:</label>
-    <datepicker id="toDatePicker" class="toDatePicker" v-model="endDate" :lower-limit="startDate"
-    :disabledDates="takenDates"/>
+  <div class="myDatePickerDiv">
+  <datepicker
+    class="myDatePicker"
+    v-model="pickedDate"
+    placeholder="Please select a date"
+    :lowerLimit="thisDay"
+    :disabledDates="{ dates: [disabledDate[0], disabledDate[1]] }"
+  />
   </div>
 </template>
-
-<script setup>
-  import Datepicker from 'vue3-datepicker'
-  import { ref } from 'vue' //Ref transmutes a value to become part of a reactive Object
-  const takenDates = ref([new Date()]); 
-  //This can be defined by already booked instances in terms of Dates
-
-  const minimumDate = ref(new Date()) //The earliest date a user can pick is Today
-  const startDate = ref(new Date()) //The date for the From datePicker - the start Date
-  const endDate = ref(new Date()) //The date for the To datePicker - the end date
-</script>
-<style>
+<style scoped>
+  .myDatePickerDiv{
+    width:max-content;
+    margin-left:auto;
+    margin-right:auto;
+  }
 </style>
