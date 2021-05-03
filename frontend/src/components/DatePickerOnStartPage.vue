b8c5b8ad62efa05765f87b4f00377822163e8046
Mikael Rusin <mikael.rusin@gmail.com>
2021-05-03 14:15:58 +0200

    Implemented Datepicker Components
    The DatePickerOnStartPage component utilzies a Datepicker
    component which was installed utilizing npm i vue3-datepicker
    
    The Documentation for said package, can be found on:
    https://icehaunter.github.io/vue3-datepicker/
    
    It sports capacity to disable upcoming dates that are not eligable, 
    disable dates on eligability criterias and is bound to be reactive
    in terms of updating values.
    
    Has a Minimum date of Today and Upper limit on Start date to be that of the end-date.

---
+++ frontend/src/components/DatePickerOnStartPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="fromDatePickerDiv">
+      <div><label for="fromDatePicker">From the:</label>
+      <datepicker id="fromDatePicker" class="fromDatePicker" v-model="startDate" :lower-limit="minimumDate" :upper-limit="endDate"/></div>
+  </div>
+  <div class="toDatePickerDiv">
+    <label for="toDatePicker">To the:</label>
+    <datepicker id="toDatePicker" class="toDatePicker" v-model="endDate" :lower-limit="startDate"/>
+  </div>
+</template>
+
+<script setup>
+  import Datepicker from 'vue3-datepicker'
+  import { ref } from 'vue' //Ref transmutes a value to become part of a reactive Object
+  
+  const minimumDate = ref(new Date()) //The earliest date a user can pick is Today
+  const startDate = ref(new Date()) //The date for the From datePicker - the start Date
+  const endDate = ref(new Date()) //The date for the To datePicker - the end date
+  
+</script>
+
+<style scoped>
+.fromDatePickerDiv, .toDatePickerDiv{
+  margin-left:auto;
+  margin-right:auto;
+  width:max-content;
+}
+</style>
