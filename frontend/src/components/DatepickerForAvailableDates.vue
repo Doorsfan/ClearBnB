cc38d47f16206c92f3c936c70c3e5b43002584f5
Mikael Rusin <mikael.rusin@gmail.com>
2021-05-03 19:11:53 +0200

    Implemeneted base for adding disabled Days

--- frontend/src/components/DatepickerForAvailableDates.vue
+++ frontend/src/components/DatepickerForAvailableDates.vue
@@ -4,10 +4,30 @@ import { ref } from 'vue'
 import { add } from 'date-fns'
 //NOTE: Datepickers are Readonly in properties, meaning you cannot directly attach CSS to them outside of built in
 // API - which only affects the input field in terms of selection of Date
-let disabledDate = ref([new Date("2021-05-08"), new Date("2021-05-09")]) //Just add dates here to have them be disabled
-//Look into making it able to parse in loop in terms of Datepicker dates
+//this.disabledDays = { dates: [new Date("2021-05-08"), new Date("2021-05-10")] } //Can utilize object literal
+//this.disabledDays.dates.push(new Date("2021-05-12")) //And add disabled dates through various means
 let thisDay = ref(new Date())
 </script>
+<script>
+export default {
+  data() {
+    return {
+      disabledDays: {
+        dates: []
+      }
+    }
+  },
+  methods: {
+    addDisabledDate(){
+      this.disabledDays.dates.push(new Date("2021-05-20"))
+    },
+    getDisabledDays(){
+      return this.disabledDays.dates
+    }
+  }
+}
+</script>
+
 
 <template>
   <div class="myDatePickerDiv">
@@ -16,8 +36,12 @@ let thisDay = ref(new Date())
     v-model="pickedDate"
     placeholder="Please select a date"
     :lowerLimit="thisDay"
-    :disabledDates="{ dates: [disabledDate[0], disabledDate[1]] }"
+    :disabledDates="disabledDays"
   />
+  </div>
+  <div>
+    <button @click="addDisabledDate" class="addDate" value="add a date">Add Date
+    </button>
   </div>
 </template>
 <style scoped>
