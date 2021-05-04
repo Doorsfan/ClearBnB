da31c4ae28dc994f9271fdaa91c0844437b90e5f
Daniel Labbé <daniel@labbe.de>
2021-05-04 14:19:22 +0200

    added Booking

--- src/main/java/models/Booking.java
+++ src/main/java/models/Booking.java
@@ -1,2 +1,20 @@
-package models;public class Booking {
+package models;
+
+import nosqlite.annotations.Document;
+import nosqlite.annotations.Id;
+
+@Document
+public class Booking {
+
+    @Id
+    private String id;
+    private String userId;
+    private String leaseId;
+    private String location;
+    private String startDate;
+    private String endDate;
+    private int guests;
+    private double totalPrice;
+
+    public Booking() {}
 }
