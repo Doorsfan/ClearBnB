db084263c7bd990a1641c03b3601e09b99a8405c
Mikael Rusin <mikael.rusin@gmail.com>
2021-05-04 10:55:55 +0200

    Added base

---
+++ frontend/src/components/DescriptionForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <textarea v-model="message" placeholder="Add a description to your Lease.."></textarea>
+  </div>
+</template>
+<script>
+  import ref from 'vue'
+  let message = ref("")
+  export default {
+    data() {
+      return {
+        message: ""
+      }
+    },
+    methods: {
+      getMessage(){
+        return this.message
+      }
+    }
+  }
+</script>
+<style scoped>
+</style>
