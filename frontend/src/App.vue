7a1e6999a6fa741d1636d9faad5fc989ecc155b9
Mikael Rusin <mikael.rusin@gmail.com>
2021-05-04 16:00:48 +0200

    Integrated base for Component to handle ImageURLs
    Integrated basic component that can add and handle ImageURLs
    
    Might have to rework this in light of uploads of images

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <ImageUrlInputForm @updatedImgURLs="updateImageURLsList" />
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+  import ImageUrlInputForm from './components/ImageUrlInputForm.vue'
 </script>
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    updateImageURLsList(updatedList){
+      console.log(updatedList)
+      console.log("Just needs to be woven into the rest of the design")
+    }
+  }
+}
+</script>
+
 
 <style>
 #app {
---
+++ frontend/src/components/ImageUrlInputForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="ImageURLSdiv">
+    <p class="ImageURLStext">Image urls</p>
+    <form @submit.prevent="addImageUrl">
+      <input v-model="imageURL" type="text" placeholder="Image URL.." />
+      <button class="addImageButton" type="submit">Add</button>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      imageURLs: [],
+      imageURL: ''
+    }
+  },
+  methods: {
+    addImageUrl(){
+      this.imageURLs.push(this.imageURL)
+      this.$emit("updatedImgURLs", this.imageURLs)
+      this.imageURL = ''
+    }
+  }
+}
+</script>
+<style scoped>
+  .ImageURLSdiv{
+    width:max-content;
+    margin-left:auto;
+    margin-right:auto;
+  }
+  .ImageURLStext{
+    margin:0px;
+    text-align:left;
+    margin-bottom:5px;
+    font-weight:bolder;
+  }
+  .addImageButton{
+    background-color:lightgreen;
+    margin-left:10px;
+  }
+</style>
