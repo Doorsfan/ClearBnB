f56ce5e854442e6614f7e511d19e11e166905f8d
Mikael Rusin <mikael.rusin@gmail.com>
2021-05-04 17:33:07 +0200

    Integrated Radio Buttons
    Just need to make certain that all components are put as a part of the App instance in the App when there is a specific View to circumvent warnings about not emiting correctly

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div>
+    <HousingRadioButtons @choseHouseType="updateHousingChosen"/>
+  </div>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+  import HousingRadioButtons from "./components/HousingRadioButtons.vue"
+</script>
+<script>
+export default {
+  data() {
+    return {
+      choseHousing: '',
+    }
+  },
+  methods: {
+    updateHousingChosen(choseHousing){
+      //Have in Add Residence View that it updates based on this function
+      console.log(choseHousing)
+    }
+  }
+}
 </script>
 
 <style>
---
+++ frontend/src/components/HousingRadioButtons.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="firstRow">
+    <input @change="updateChosenHousing" class="buttonForHouse" type="radio" id="House" value="House" v-model="chosenHouseType">
+    <label class="labelForHouse" for="House">House</label>
+    <input @change="updateChosenHousing" class="buttonForFlat" type="radio" id="Flat" value="Flat" v-model="chosenHouseType">
+    <label class="labelForFlat" for="Flat">Flat</label>
+    <input @change="updateChosenHousing" class="buttonForOther" type="radio" id="Other" value="Other" v-model="chosenHouseType">
+    <label class="labelForOther" for="Other">Other</label>
+  </div>
+  <div class="secondRow">
+    <input @change="updateChosenHousing" class="buttonForEntire" type="radio" id="Entire" value="Entire residence" v-model="chosenHouseType">
+    <label class="labelForEntire" for="Entire">Entire residence</label>
+    <input @change="updateChosenHousing" class="buttonForPart" type="radio" id="Part" value="Part" v-model="chosenHouseType">
+    <label class="labelForPart" for="Part">Part</label>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      chosenHouseType: ''
+    }
+  },
+  methods: {
+    updateChosenHousing() {
+      console.log(this.chosenHouseType);
+      this.$emit("chosenHouseType", this.chosenHouseType)
+    }
+  }
+}
+</script>
+<style scoped>
+  .firstRow{
+    width:210px;
+    margin-left:auto;
+    margin-right:auto;
+    padding-right:10px;
+  }
+  .secondRow{
+    width:210px;
+    margin-left:auto;
+    margin-right:auto;
+    padding-right:10px;
+  }
+  label{
+    margin-right: 5px;
+    margin-left: 5px;
+  }
+
+</style>
