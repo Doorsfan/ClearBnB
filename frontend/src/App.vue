5ab5b27a64b4fcf168630ea215f9d6c834b5d7a7
Mikael Rusin <mikael.rusin@gmail.com>
2021-05-06 00:46:28 +0200

    Implemented Base for Lease Box Displays
    After struggling with implementations of a Carousel, i accepted the notion of settling for just image displays as a middle-ground for later on integrations.
    
    Integrated Image Display boxes based on JSON values.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,78 @@
 <template>
   <div>
-    <Header />
-    <router-view></router-view>
-    <TestingFetches />
+    <LeaseDisplayBox 
+      v-for="(leaseItem, index) of leases"
+      :key="index"
+      :lease="leaseItem"/>
   </div>
 </template>
 
-<script setup>
-import Header from "./components/Header.vue";
-//import HelloWorld from "./components/HelloWorld.vue";
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
-import TestingFetches from "./components/TestingFetches.vue";
+<script setup="">
+import LeaseDisplayBox from "./components/Start View/LeaseDisplayBox.vue"
+import Lease from './components/Lease.vue'
 </script>
-
+<script>
+export default {
+  name: "App",
+  components: {
+    Lease,
+  },
+  data() {
+    return {
+      leases: [
+        {
+    "ownerId": "tsongest2@howstuffworks.com",
+    "title": "Two-bedroom vila not far from the beach",
+    "location": "Bellevue Island",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ipsum mi, finibus ac placerat congue, placerat a enim. Phasellus eleifend, nulla et tincidunt pharetra, metus magna venenatis magna, quis mollis turpis tortor eget quam. Curabitur porta enim non augue auctor, vel bibendum sem iaculis. Nam orci mi, hendrerit quis pulvinar sed, rutrum nec lorem. Pellentesque euismod purus fringilla, mollis odio quis, fringilla purus. Aliquam erat volutpat. Aliquam id neque vel massa mattis hendrerit. Vestibulum finibus egestas eleifend. Etiam ut erat rutrum, maximus odio porta, euismod est.",
+    "typeOfHousing": "Vila",
+    "startDate": "2021-05-27",
+    "endDate": "2021-07-30",
+    "pricePerNight": 75,
+    "maxGuests": 4,
+    "beds": 4,
+    "amenities": {
+      "wifi": true,
+      "kitchen": true,
+      "washer": true,
+      "heating": true,
+      "airConditioner": false
+    },
+    "imageURLs": [
+      "https://images.unsplash.com/photo-1566195992011-5f6b21e539aa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80",
+      "https://images.unsplash.com/photo-1595521624742-47e90260edab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
+    ]
+  },
+        {
+          ownerId: "balder0@fda.gov",
+          title: "Lovely cottage",
+          location: "Bellevue Island",
+          description: "Cosy 4 room cottage in central Bellevue. The cottage is located in the wonderful district, St Palmas with city nearby as well as green parks, local shops, and restaurants. Within walking distance to the heart of Bellevue. You will have the whole cottage for yourself.",
+          typeOfHousing: "Cottage",
+          startDate: "2021-05-27",
+          endDate: "2021-07-30",
+          pricePerNight: 45,
+          maxGuests: 4,
+          beds: 3,
+          amenities: {
+            wifi: true,
+            kitchen: true,
+            washer: true,
+            heating: true,
+            airConditioner: false
+          },
+          imageURLs: [
+          "https://images.unsplash.com/photo-1588046130717-0eb0c9a3ba15?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1438&q=80",
+          "https://images.unsplash.com/photo-1598204326847-aeedbc139508?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80"
+          ]
+        }
+      ],
+    }
+  }
+}
+</script>
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
---
+++ frontend/src/components/Start View/LeaseDisplayBox.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="mainBoxDiv">
+    <div class="titleDiv">{{ lease.title }}</div>
+    <div class="leaseBoxDiv">
+      <img class="imageBox" :src="lease.imageURLs[0]"><div class="secondDiv"><p>{{ lease.description }}</p></div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["lease"],
+  data() {
+    return {
+    }
+  },
+  methods: {
+    temp(){
+      console.log("hi")
+    }
+  }
+}
+</script>
+<style scoped>
+.imageBox{
+  height:180px;
+  width: 150px;
+  margin-top:5px;
+  margin-bottom:5px;
+}
+.mainBoxDiv{
+  margin-left:auto;
+  margin-right:auto;
+  width:500px;
+  margin-top:10px;
+  margin-bottom:10px;
+}
+.titleDiv{
+  text-align:center;
+}
+.leaseBoxDiv{
+  height:109px;
+  width:200px;
+  display:inline;
+}
+.secondDiv{
+  display:inline;
+  height:109px;
+  width:300px;
+  margin:0px;
+  padding:0px;
+  position:absolute;
+}
+p{
+  margin:2px;
+  margin-left: 30px;
+  margin-top:5px;
+  word-wrap:break-word; 
+  height:180px;
+  overflow:scroll;
+}
+</style>
