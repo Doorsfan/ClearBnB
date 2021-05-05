4eb71b0906066bfc91ec50a2f25524399e1c98a7
Daniel Labbé <daniel@labbe.de>
2021-05-05 13:11:58 +0200

    added required

--- frontend/src/components/ImageUrlInputForm.vue
+++ frontend/src/components/ImageUrlInputForm.vue
@@ -2,7 +2,12 @@
   <div class="ImageURLSdiv">
     <p class="ImageURLStext">Image urls</p>
     <form @submit.prevent="addImageUrl">
-      <input v-model="imageURL" type="text" placeholder="Image URL.." />
+      <input
+        v-model="imageURL"
+        type="text"
+        placeholder="Image URL.."
+        required
+      />
       <button class="addImageButton" type="submit">Add</button>
     </form>
   </div>
@@ -12,32 +17,32 @@ export default {
   data() {
     return {
       imageURLs: [],
-      imageURL: ''
-    }
+      imageURL: "",
+    };
   },
   methods: {
-    addImageUrl(){
-      this.imageURLs.push(this.imageURL)
-      this.$emit("updatedImgURLs", this.imageURLs)
-      this.imageURL = ''
-    }
-  }
-}
+    addImageUrl() {
+      this.imageURLs.push(this.imageURL);
+      this.$emit("updatedImgURLs", this.imageURLs);
+      this.imageURL = "";
+    },
+  },
+};
 </script>
 <style scoped>
-  .ImageURLSdiv{
-    width:max-content;
-    margin-left:auto;
-    margin-right:auto;
-  }
-  .ImageURLStext{
-    margin:0px;
-    text-align:left;
-    margin-bottom:5px;
-    font-weight:bolder;
-  }
-  .addImageButton{
-    background-color:lightgreen;
-    margin-left:10px;
-  }
+.ImageURLSdiv {
+  width: max-content;
+  margin-left: auto;
+  margin-right: auto;
+}
+.ImageURLStext {
+  margin: 0px;
+  text-align: left;
+  margin-bottom: 5px;
+  font-weight: bolder;
+}
+.addImageButton {
+  background-color: lightgreen;
+  margin-left: 10px;
+}
 </style>
