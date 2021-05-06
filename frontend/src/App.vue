47b4bf49acc9784f6565cad32a1415922422c378
Mikael Rusin <mikael.rusin@gmail.com>
2021-05-06 12:13:50 +0200

    Base integration of DateForm

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
-    <LocationForm />
+    <LocationForm @changedLocation="filterBasedOnLocation"/>
   </div>
 </template>
 
 <script setup>
-  import LocationForm from './components/LocationForm.vue'
+  import LocationForm from './components/DateForm.vue'
 </script>
+<script>
+export default {
+  data() {
+    return {
+      relevantLeases: [],
+      allLeases: []
+    }
+  },
+  methods:{
+    filterBasedOnLocation(newLocation){
+      this.relevantLeases = []
+      for(let lease of allLeases){
+        if(lease.location.includes(newLocation)){
+          this.relevantLeases.push(lease);
+        }
+      }
+    }
+  }
+}
+</script>
+
 
 <style>
 #app {
---
+++ frontend/src/components/DateForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="mainDiv">
+    Lease From:
+    <input @change="updateWantedLocation" type="text" :placeholder="today" class="locationInput" v-model="wantedLocation">
+  </div>
+</template>
+<script>
+let base = new Date();
+let month = base.getMonth()+1;
+let day = base.getDate();
+let zeroBeforeMonth = '';
+let zeroBeforeDay = '';
+if(month < 10){
+  zeroBeforeMonth += '0' + month
+  month = zeroBeforeMonth
+}
+if(day < 10){
+  zeroBeforeDay += '0' + day
+  day = zeroBeforeDay
+}
+  export default {
+    data() {
+      return {
+        wantedLocation: '',
+        today: base.getFullYear()+'-'+month+'-'+day
+      }
+    },
+    methods: {
+      updateWantedLocation(){
+        this.$emit("changedLocation", this.wantedLocation)
+      }
+    }
+  }
+</script>
+<style scoped>
+.locationInput{
+  width:70px;
+  padding: 2px;
+}
+</style>
