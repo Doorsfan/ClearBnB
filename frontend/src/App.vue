3ab59de67a736e48f2acdfcded9595200236e8c9
Daniel Labbé <daniel@labbe.de>
2021-05-07 15:35:22 +0200

    fixed hamburger styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
+    <HamburgerMenu />
     <Header />
     <router-view></router-view>
-    <HamburgerMenu />
   </div>
 </template>
 
@@ -28,7 +28,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
--- frontend/src/components/HamburgerMenu.vue
+++ frontend/src/components/HamburgerMenu.vue
@@ -5,13 +5,13 @@
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
     />
+    <em @click="operateHamburger" class="fa fa-bars"></em>
     <div id="router-links">
       <p><router-link to="/">Home</router-link></p>
       <p><router-link to="/userPage">User Page</router-link></p>
       <p><router-link to="/login">Log in</router-link></p>
       <p><router-link to="/signUp">Sign up</router-link></p>
     </div>
-    <em @click="operateHamburger" class="fa fa-bars"></em>
   </div>
 </template>
 
@@ -37,14 +37,11 @@ export default {
 }
 .hamburger {
   overflow: hidden;
-  background-color: #333;
   position: relative;
 }
 .fa {
-  background: black;
   display: block;
-  position: absolute;
-  right: 0;
+  left: 0;
   top: 0;
 }
 </style>
