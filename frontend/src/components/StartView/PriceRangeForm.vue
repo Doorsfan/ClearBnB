596eea2e2a725196edf805c3eea4c1460567f201
Mikael Rusin <mikael.rusin@gmail.com>
2021-05-09 00:22:45 +0200

    Integrated Dynamic Filters
    In light of Filters needing to dynamically be able to update based on the state of the other factors - I had to manually map out every single state schematic in terms of different variables states - To cover all cases in terms of filters to process each situation.
    
    The entire reason for this, was to make it so that every possible state was mapped - And full coverage across the state machine was reached - in regards to being able to cover all search cases.
    
    One could re-factor the code and shorten it down - Or utilize more advanced javascript to boil down the repetition away - But this is a first draft of the setup, having all of the filters interact and depend on each other.

---
+++ frontend/src/components/StartView/PriceRangeForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="priceRangeFormDiv">
+    Min Price: <input class="minPriceInputForm" type="text" v-model="minPriceWanted" placeholder="Min Price"> Max Price: <input class="maxPriceInputForm" type="text" v-model="maxPriceWanted" placeholder="Max Price">
+  </div>
+</template>
+<script>
+export default {
+  emits: ['updateMyMinPrice', 'updateMyMaxPrice'],
+  data() {
+    return {
+      minPriceWanted: '',
+      maxPriceWanted: ''
+    }
+  },
+  watch: {
+    minPriceWanted(){
+      this.$emit("updateMyMinPrice", this.minPriceWanted)
+    },
+    maxPriceWanted(){
+      this.$emit("updateMyMaxPrice", this.maxPriceWanted)
+    }
+  }
+}
+</script>
+<style scoped>
+input{
+  width:60px;
+  margin:3px;
+}
+.priceRangeFormDiv{
+  display:inline;
+  width:max-content;
+  margin-left:20px;
+}
+</style>
