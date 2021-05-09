c975c5054e7cf9a9d87e9dd52ed1f35e070605e6
Mikael Rusin <mikael.rusin@gmail.com>
2021-05-09 16:54:21 +0200

    Added ref

--- frontend/src/components/DetailView/DatePickerForLease.vue
+++ frontend/src/components/DetailView/DatePickerForLease.vue
@@ -4,9 +4,9 @@
 </template>
 <script setup="">
   import Datepicker from 'vue3-datepicker'
-  import ref from 'vue'
 </script>
 <script>
+import { ref } from 'vue' //Ref transmutes a value to become part of a reactive Object
 let chosenDate = ref(new Date())
 export default {
   props: ["relevantLease", "allBookings"],
