3cf91dffdac50a7c86614dfd3715b223ace7addc
Mikael Rusin <mikael.rusin@gmail.com>
2021-05-09 13:36:57 +0200

    Laid base for DatePickerForLease

---
+++ frontend/src/components/DetailView/DatePickerForLease.vue
@@ -0,0 +1,15 @@
+<template>
+  <Datepicker :disabledDays="bookedDays"/>
+</template>
+<script setup="">
+  import Datepicker from 'vue3-datepicker'
+</script>
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+
+}
+</script>
