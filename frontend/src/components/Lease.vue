f1a924f34426c4da31cf2aa5a5b1bf989169b343
Mikael Rusin <mikael.rusin@gmail.com>
2021-05-11 09:09:43 +0200

    Integrated User class, Lease class and User Bookings List

--- frontend/src/components/Lease.vue
+++ frontend/src/components/Lease.vue
@@ -2,11 +2,24 @@
 </template>
 
 <script>
-export default {
+export default class Lease {
+  constructor(title, location, description, typeOfHousing, startDate, endDate, pPPN, maxGuests, beds, amenities, imageURLs){
+    this.title = title
+    this.location = location
+    this.description = description
+    this.typeOfHousing = typeOfHousing
+    this.startDate = startDate
+    this.endDate = endDate
+    this.pPPN = pPPN
+    this.maxGuests = maxGuests
+    this.beds = beds
+    this.amenities = amenities
+    this.imageURLs = imageURLs
+  }
   data() {
     return {
-      title: '',
-      Description: '',
+      title: this.title,
+      Description: this.location,
       image: ''
     }
   }
---
+++ frontend/src/components/User.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+<script>
+export default class User{
+  constructor(myBookings){
+    this.myBookings = myBookings;
+  }
+
+  getBookings(){
+    return this.myBookings
+  }
+  addBooknig(newBooking){
+    this.myBookings.push(newBooking);
+  }
+}
+</script>
+<style scoped>
+</style>
