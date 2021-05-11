567e9d9fdcfbd5582401926c5e3680162407b917
WanjaMW <80105300+WanjaMW@users.noreply.github.com>
2021-05-11 19:47:18 +0200

    Added scoped css

--- frontend/src/views/BookingView.vue
+++ frontend/src/views/BookingView.vue
@@ -1,8 +1,9 @@
 <template>
+  <div class="booking-view-app-main">
   <div class="booking-view-container">
     <div>
       <div class="location-maxguests">
-      <h2>{{ lease.title }}</h2>
+      <h1>{{ lease.title }}</h1>
       <p> {{ lease.location }} | Maximum {{ lease.maxGuests }} guests</p>
       </div>
       <div class="booking-view-images-container">
@@ -19,8 +20,8 @@
       <div class="booking-view-description">
         <p> {{ lease.description}} </p>
       </div>
-      <p><b>Amenities</b></p>
-      <p>*** Insert component ***</p>
+      <div class="amenities">Amenities
+      <p>*** Insert component ***</p></div>
       <!-- 
         Insert component Amenities.vue
        -->
@@ -41,7 +42,7 @@
 
     </div>
   </div>
-  <footer>Insert footer component</footer>
+  </div>
 </template>
 
 <script setup="">
@@ -50,11 +51,10 @@
 
 <script>
 import leases from '../../json_temp/leases.json'
-import DatePickerOnStartpage from '../components/DatePickerOnStartPage.vue'
+import DatePickerOnStartpage from '../components/StartView/DatePickerOnStartPage.vue'
 export default  {
 data()  {
   return  {
-    image: { backgroundImage: "url(https://images.unsplash.com/photo-1573088870079-67b4526aa950?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80);" },
     lease: null
   }
 },
@@ -72,42 +72,15 @@ created() {
 }
 </script>
 
-<style>
+<style scoped>
+
 @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Lobster&family=Merriweather+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Raleway:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap');
 
-/* header{
-  height: 60px;
-  background: linear-gradient(
-    rgb(121, 167, 182),
-    rgb(218, 248, 248)
-  );
-  padding: 20px;
-  font-family: Merriweather Sans;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  text-decoration: none;
-  font-weight: 700;
-  line-height: 140%;
-  border-bottom: solid 2px rgb(218, 248, 248);
-}
-a {
-  text-decoration: none;
-  color: #2c3e50;
-} */
-
-#app {
-  background-color: rgb(218, 248, 248);
-  font-family: Merriweather Sans;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  .booking-view-app-main  {
   background-image: url(https://images.unsplash.com/photo-1573088870079-67b4526aa950?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80);
   background-size: cover;
   background-position: center;
-
+  flex-grow: 1;
   display: flex;
   flex-direction: column;
   min-height: 100vh;
@@ -120,25 +93,35 @@ select  {
   background-color:rgba(234, 241, 241);
   margin-right: 10px;
 }
+
 image {
   width: 100%;
   object-fit: cover; 
   object-position: center;
-}
+} 
 
 .booking-view-container{
-  background-color: rgba(234, 241, 241, 0.7);
+  background-color: rgba(218, 224, 224, 0.8);
   display: flex;
   flex-direction: column;
   align-items: center;
+  min-height: 100vh;
   width: 80%;
-  margin: auto;
-  padding: 10px 30px;
+  margin: 0 auto;
+  padding: 30px;
+  font-family: Merriweather Sans;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: rgb(10, 10, 10);
 }
 
 .location-maxguests{
-  line-height: 60%;
   font-weight: 700;
+  padding: 30px 0 20px;
+}
+.location-maxguests h1 {
+  padding: 30px 0 10px;
 }
 
 .booking-view-images-container{
@@ -150,14 +133,13 @@ image {
 .booking-view-image{
   max-height: 260px;
   margin: 10px;
-  box-shadow: 5px 5px 5px 0 rgb(131, 154, 161);
+  box-shadow: 5px 5px 5px 0 rgb(136, 154, 160);
   border-radius: 4px;
 }
 
 .type-pppn{
-  line-height: 60%;
-  padding: 0;
   font-weight: 700;
+  padding: 10px 0 20px;
 }
 
 .booking-view-description{
@@ -165,11 +147,12 @@ image {
   padding: 0 50px;
   line-height: 140%;
 }
-
-footer{
-  height: 40px;
+.amenities{
+  font-weight: 700;
 }
-
+.amenities p{
+  padding: 10px 0 20px;
+}
 
 </style>
 
