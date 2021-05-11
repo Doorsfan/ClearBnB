7b435e94417350165073982dc25a583e74a88902
Daniel Labbé <daniel@labbe.de>
2021-05-11 16:32:54 +0200

    work in progress

--- frontend/src/components/PPPN.vue
+++ frontend/src/components/PPPN.vue
@@ -4,43 +4,50 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      startDate: "2021-05-10", //mock data
-      endDate: "2021-06-15", //mock data
-      days: 5, //mock data
-      price: 10, //mock data
-      dateRange: this.computeDateRange(this.startDate, this.endDate),
-      totalPrice: this.computeCost(this.days, this.price),
-    };
-  },
-  methods: {
-    computeDateRange(startDate, endDate) {
-      startDate = this.convertToDate(startDate);
-      endDate = this.convertToDate(endDate);
+//computeDateRange(this.startDate, this.endDate)
+//computeCost(this.days, this.price)
 
-      let dateDifferenceInMs = endDate.getTime() - startDate.getTime();
-      let days = dateDifferenceInMs / (1000 * 60 * 60 * 24);
+export default class PPPN {
+  startDate = "2021-05-10"; //mock data
+  endDate = "2021-06-15"; //mock data
+  days = 5; //mock data
+  price = 10; //mock data
+  totalPrice = 0;
 
-      return days;
-    },
-    convertToDate(isoDate) {
-      let year = isoDate.substring(0, 4);
-      let month = isoDate.substring(6, 8);
-      let day = isoDate.substring(9, 11);
-      let date = new Date();
-      date.setFullYear(year);
-      date.setMonth(month);
-      date.setDate(day);
+  constructor(startDate, endDate, days, price, totalPrice) {
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.days = days;
+    this.price = price;
+    this.totalPrice = totalPrice;
+  }
 
-      return date;
-    },
-    computeCost(days, price) {
-      return days * price;
-    },
-  },
-};
+  computeDateRange(startDate, endDate) {
+    startDate = this.convertToDate(startDate);
+    endDate = this.convertToDate(endDate);
+
+    let dateDifferenceInMs = endDate.getTime() - startDate.getTime();
+    let days = dateDifferenceInMs / (1000 * 60 * 60 * 24);
+
+    return days;
+  }
+
+  convertToDate(isoDate) {
+    let year = isoDate.substring(0, 4);
+    let month = isoDate.substring(6, 8);
+    let day = isoDate.substring(9, 11);
+    let date = new Date();
+    date.setFullYear(year);
+    date.setMonth(month);
+    date.setDate(day);
+
+    return date;
+  }
+
+  computeCost(days, price) {
+    return days * price;
+  }
+}
 </script>
 
 <style>
