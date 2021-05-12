a8d59be90f83ce40042c6220634fae31aeeaed80
Daniel Labbé <daniel@labbe.de>
2021-05-12 09:09:52 +0200

    added to bookinview to header

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="navbar">
     <div class="left">
-    <router-link to="/">Home</router-link>
+      <router-link to="/">Home</router-link>
     </div>
     <div class="center">
-    <router-link to="userPage">User Page</router-link>
-    <router-link to="AddLease">Add Lease</router-link>
+      <router-link to="userPage">User Page</router-link>
+      <router-link to="AddLease">Add Lease</router-link>
+      <router-link to="BookingView">Booking View</router-link>
     </div>
     <div class="right">
-    <router-link to="signUp"><i class="fas fa-user-plus"></i>Sign Up</router-link>
-    <span>|</span>
-    <router-link to="login"><i class="fas fa-sign-in-alt"></i>Login</router-link>
+      <router-link to="signUp"
+        ><i class="fas fa-user-plus"></i>Sign Up</router-link
+      >
+      <span>|</span>
+      <router-link to="login"
+        ><i class="fas fa-sign-in-alt"></i>Login</router-link
+      >
     </div>
   </div>
 </template>
@@ -26,7 +31,7 @@ export default {};
   -moz-osx-font-smoothing: grayscale;
   display: flex;
   flex-direction: row;
-  justify-content:space-between;
+  justify-content: space-between;
   border-bottom: 0.1rem solid #00200c;
   height: 7rem;
   align-items: center;
@@ -36,34 +41,33 @@ a {
   font-size: 1.7rem;
   text-decoration: none;
   color: rgb(63, 63, 63);
-
 }
-.center a{
-margin: 0 1rem;
-font-weight: bold;
+.center a {
+  margin: 0 1rem;
+  font-weight: bold;
 }
-.right{
+.right {
   align-self: flex-start;
   margin-top: 1rem;
   display: flex;
   flex-direction: row;
   align-items: baseline;
 }
-.right span{
+.right span {
   font-size: 1.3rem;
   margin: 0 1rem;
   color: rgb(172, 172, 172);
 }
-.right a{
+.right a {
   font-size: 1.3rem;
   color: rgb(172, 172, 172);
   transition: color 0.3s;
 }
-.right i{
+.right i {
   font-size: 1.3rem;
   margin-right: 0.5rem;
 }
-.right a:hover{
+.right a:hover {
   color: rgb(112, 112, 112);
 }
 </style>
