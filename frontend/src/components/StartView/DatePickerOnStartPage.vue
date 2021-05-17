707c7390754f7b70598dd31d458c998538027373
Daniel Labbé <daniel@labbe.de>
2021-05-17 15:15:43 +0200

    removed upper limit on datepicker:from

--- frontend/src/components/StartView/DatePickerOnStartPage.vue
+++ frontend/src/components/StartView/DatePickerOnStartPage.vue
@@ -1,33 +1,43 @@
 <template>
-<div class="datePickersMainDiv">
-  <div class="firstDiv">
-    <label for="fromDatePicker">From the:</label>
-    <Datepicker id="fromDatePicker" class="fromDatePicker" v-model="startDate" :lower-limit="minimumStartDate" :upper-limit="endDate"/>
+  <div class="datePickersMainDiv">
+    <div class="firstDiv">
+      <label for="fromDatePicker">From the:</label>
+      <Datepicker
+        id="fromDatePicker"
+        class="fromDatePicker"
+        v-model="startDate"
+        :lower-limit="minimumStartDate"
+      />
+    </div>
+    <div class="secondDiv">
+      <label for="toDatePicker">To the:</label>
+      <Datepicker
+        id="toDatePicker"
+        class="toDatePicker"
+        v-model="endDate"
+        :lower-limit="minimumEndDate"
+      />
+    </div>
   </div>
-  <div class="secondDiv">
-    <label for="toDatePicker">To the:</label>
-    <Datepicker id="toDatePicker" class="toDatePicker" v-model="endDate" :lower-limit="minimumEndDate"/>
-  </div>
-</div>
 </template>
 
 <script setup>
-  import Datepicker from 'vue3-datepicker'
+import Datepicker from "vue3-datepicker";
 </script>
 <script>
-import { ref } from 'vue' //Ref transmutes a value to become part of a reactive Object
-  
-const minimumDate = ref(new Date()) //The earliest date a user can pick is Today
-const startDate = ref(new Date()) //The date for the From datePicker - the start Date
+import { ref } from "vue"; //Ref transmutes a value to become part of a reactive Object
+
+const minimumDate = ref(new Date()); //The earliest date a user can pick is Today
+const startDate = ref(new Date()); //The date for the From datePicker - the start Date
 
 //To make the Date be able to just add 1 day, we can define a function on it's Prototype
-Date.prototype.addDays = function(days) {
+Date.prototype.addDays = function (days) {
   var date = new Date(this.valueOf());
   date.setDate(date.getDate() + days);
   return date;
-}
+};
 let endDateSevenDaysAhead = new Date().addDays(7);
-const endDate = ref(endDateSevenDaysAhead) //The date for the To datePicker - the end date
+const endDate = ref(endDateSevenDaysAhead); //The date for the To datePicker - the end date
 
 export default {
   //emits: ['updateEndDate', 'updateStartDate'],
@@ -36,37 +46,38 @@ export default {
       minimumStartDate: new Date(),
       minimumEndDate: new Date().addDays(1),
       startDate: startDate,
-      endDate: endDate
-    }
+      endDate: endDate,
+    };
   },
   watch: {
-    startDate(){ //Keeps a track of when startDate has changed
-      let convertedDate = this.convertDate(this.startDate)
-      this.$emit("updateStartDate", convertedDate)
+    startDate() {
+      //Keeps a track of when startDate has changed
+      let convertedDate = this.convertDate(this.startDate);
+      this.$emit("updateStartDate", convertedDate);
     },
-    endDate(){
-      let convertedDate = this.convertDate(this.endDate)
-      this.$emit("updateEndDate", convertedDate)     
-    }
+    endDate() {
+      let convertedDate = this.convertDate(this.endDate);
+      this.$emit("updateEndDate", convertedDate);
+    },
   },
-  methods: {  
-    convertDate(dateToConvert){
+  methods: {
+    convertDate(dateToConvert) {
       let month = dateToConvert.getMonth() + 1;
       let day = dateToConvert.getDate();
-      let zeroBeforeMonth = '';
-      let zeroBeforeDay = '';
-      if(month < 10){
-        zeroBeforeMonth += '0' + month
-        month = zeroBeforeMonth
+      let zeroBeforeMonth = "";
+      let zeroBeforeDay = "";
+      if (month < 10) {
+        zeroBeforeMonth += "0" + month;
+        month = zeroBeforeMonth;
       }
-      if(day < 10){
-        zeroBeforeDay += '0' + day
-        day = zeroBeforeDay
+      if (day < 10) {
+        zeroBeforeDay += "0" + day;
+        day = zeroBeforeDay;
       }
-      return dateToConvert.getFullYear() + '-' + month + '-' + day
-    } 
-  }
-}
+      return dateToConvert.getFullYear() + "-" + month + "-" + day;
+    },
+  },
+};
 </script>
 
 
@@ -74,9 +85,9 @@ export default {
 * {
   font-size: 20px;
 }
-div{
-  width:max-content;
-  display:inline-block;
+div {
+  width: max-content;
+  display: inline-block;
   margin: 10px;
 }
 </style>
