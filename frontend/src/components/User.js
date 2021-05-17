f6929680c72b55657b9276e02f90aa5bd689f958
Mikael Rusin <mikael.rusin@gmail.com>
2021-05-17 14:24:46 +0200

    removed userInfo id on User

--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -1,19 +1,12 @@
 export default class User {
-  constructor(username, password, userInfo) {
+  constructor(username, password) {
     this.username = username;
     this.password = password;
-    this.userInfo = userInfo;
   }
   getUsername() {
     return this.username;
   }
   setUsername(newUsername) {
     this.username = newUsername;
-  }
-  getUserInfo() {
-    return this.userInfo;
-  }
-  setUserInfo(newUserInfo) {
-    this.userInfo = newUserInfo;
   }
 }
--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -14,15 +14,13 @@ public class User {
   private String id;
   private String username;
   private String password;
-  private String userInfo;
 
   // default constructor
   public User() { }
 
-  public User(String username, String password, String userInfo) {
+  public User(String username, String password) {
     this.username = username;
     this.password = password;
-    this.userInfo = userInfo;
   }
 
   @Override
@@ -31,7 +29,6 @@ public class User {
             "id='" + id + '\'' +
             ", username='" + username + '\'' +
             ", password='" + password + '\'' +
-            ", userInfo=" + userInfo +
             '}';
   }
 
@@ -57,13 +54,5 @@ public class User {
 
   public void setPassword(String password) {
     this.password = password;
-  }
-
-  public String getUserInfo() {
-    return userInfo;
-  }
-
-  public void setUserInfo(String userInfo) {
-    this.userInfo = userInfo;
   }
 }
