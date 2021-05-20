b9b3f59be01b3177b137cc11fd6c4007203c5d1d
WanjaMW <80105300+WanjaMW@users.noreply.github.com>
2021-05-20 03:26:38 +0200

    Fixed css on BV, WOP

--- frontend/src/views/BookingView.vue
+++ frontend/src/views/BookingView.vue
@@ -7,15 +7,17 @@
         <p> {{ location }} | Maximum {{ maxGuests }} guests</p>
         </div>
         <div class="booking-view-images-container">
-          <img class="booking-view-image" :src="imageURLs[0]" /> 
-          <img class="booking-view-image" :src="imageURLs[1]" /> 
+          <img class="booking-view-image-1" :src="imageURLs[0]" /> 
+          <img class="booking-view-image-2" :src="imageURLs[1]" />
+          <img class="booking-view-image-3" :src="imageURLs[2]" />
+          <img class="booking-view-image-4" :src="imageURLs[3]" />
         </div>
       <!-- <Carousel :myLease="lease" /> -->
       <!-- <div class="lease-images">
         {{lease.imageURLs[0]}} {{lease.imageURLs[1]}}
       </div> -->
         <div class="type-price">
-          <p>{{ typeOfHousing }} | Per person per night: {{ Math.round(price * 1.15) }} SEK</p>
+          <p>{{ typeOfHousing }} | Per person per night: {{ Math.round(price * 1.15) }} Euro</p>
         </div>
         <div class="booking-view-description">
           <p> {{ description}} </p>
@@ -26,17 +28,18 @@
         Insert component for Amenities
        -->
 
-        <p>
+        
        <!-- Edit with data from leases.json --> 
           <select v-model="selectedNumberOfGuests"> 
             <option disabled value="">Select number of guests</option>
             <option>1</option>
             <option>2</option>
             <option>3</option>
-          </select> guests
-        </p>
-      
+          </select>guests
+    
+      <div class="myDatePickerDiv">
       <DatepickerForAvailableDates />
+      </div>
       </div>
     </div>
   </div>
@@ -110,21 +113,15 @@ select  {
   margin-right: 10px;
 }
 
-image {
-  width: 100%;
-  object-fit: cover; 
-  object-position: center;
-} 
-
 .booking-view-container{
   background-color: rgba(218, 224, 224, 0.8);
   display: flex;
   flex-direction: column;
   align-items: center;
   min-height: 100vh;
-  width: 80%;
+  width: 90%;
   margin: 0 auto;
-  padding: 30px;
+  padding: 50px;
   font-family: Merriweather Sans;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -134,21 +131,50 @@ image {
 
 .location-maxguests{
   font-weight: 700;
-  padding: 30px 0 20px;
+  padding: 30px 0 30px;
 }
 .location-maxguests h1 {
-  padding: 30px 0 10px;
+  padding: 0 0 10px;
 }
 
 .booking-view-images-container{
   display: inline-block;
   align-items: center;
-  max-width: 80%;
 }
 
-.booking-view-image{
-  max-height: 260px;
-  margin: 10px;
+.booking-view-image-1{
+  width: 50%;
+  height: 300px;
+  object-fit: cover;
+  object-position: center;
+  margin: 0 10px 10px 10px;
+  box-shadow: 5px 5px 5px 0 rgb(136, 154, 160);
+  border-radius: 4px;
+}
+.booking-view-image-2{
+  width: 30%;
+  height: 300px;
+  object-fit: cover;
+  object-position: center;
+  margin: 0 10px 10px 10px;
+  box-shadow: 5px 5px 5px 0 rgb(136, 154, 160);
+  border-radius: 4px;
+}
+.booking-view-image-3{
+  width: 30%;
+  height: 300px;
+  object-fit: cover;
+  object-position: center;
+  margin: 0 10px 10px 10px;
+  box-shadow: 5px 5px 5px 0 rgb(136, 154, 160);
+  border-radius: 4px;
+}
+.booking-view-image-4{
+  width: 50%;
+  height: 300px;
+  object-fit: cover;
+  object-position: center;
+  margin: 0 10px 10px 10px;
   box-shadow: 5px 5px 5px 0 rgb(136, 154, 160);
   border-radius: 4px;
 }
@@ -160,11 +186,16 @@ image {
 
 .booking-view-description{
   text-align: justify;
-  padding: 0 50px;
+  padding: 0 90px;
   line-height: 140%;
+  font-size: 110%;
+}
+.myDatePickerDiv{
+  width: 400px;
 }
 .amenities{
   font-weight: 700;
+  padding: 10px;
 }
 .amenities p{
   padding: 10px 0 20px;
