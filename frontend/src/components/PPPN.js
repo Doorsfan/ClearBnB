7b00f8b0bd34beb8b55fc3459e64bc3e2a60a591
Mikael Rusin <mikael.rusin@gmail.com>
2021-05-20 23:36:26 +0200

    Integrated so that a user can book through Booking View
    Integrated so that Booking View actually can allow for real booking - saving profits - and forcing user to be logged in/choose valid dates to book
    
    Also integrated displayed price to pay for a ocassion based on days/people/PPPN
    
    Sloped Carousel in Booking View
    
    Booked dates are already disabled, but can integrate so that it accounts for this when booking regardless

---
+++ frontend/src/components/PPPN.js
@@ -0,0 +1,44 @@
+export default class PPPN {
+
+  constructor() {}
+  
+  computeDateRange(startDate, endDate) {
+    let dateDifferenceInMs = endDate.getTime() - startDate.getTime();
+    let days = dateDifferenceInMs / (1000 * 60 * 60 * 24);
+    return days;
+  }
+  addDays(wantedDate, days) {
+    var date = new Date(wantedDate.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+  }
+  getCorrectDateFormat(wantedDate){
+      let year = wantedDate.getFullYear();
+      let month = wantedDate.getMonth() + 1;
+      let day = wantedDate.getDate();
+      if(month < 10){
+        month = '0' + month;
+      }
+      if(day < 10){
+        day = '0' + day;
+      }
+      return year + '-' + month + '-' + day;
+  }
+
+  convertToDate(isoDate) {
+    isoDate = isoDate.toString();
+    console.log("The iso String was: ", isoDate);
+    let day = isoDate.substring(0, 4);
+    let month = isoDate.substring(6, 8);
+    let year = isoDate.substring(9, 11);
+    console.log("year was: ", year);
+    console.log("month was: ", month);
+    console.log("day was : ", day);
+    let date = new Date(year, month, day);
+    console.log("At the end, date was: ", date);
+    return date;
+  }
+  computeCost(days, price) {
+    return days * price;
+  }
+}
