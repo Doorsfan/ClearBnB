ca2c599795c89bf617a9801a29d601121d514f7e
Mikael Rusin <mikael.rusin@gmail.com>
2021-05-25 15:16:25 +0200

    Fixed the Arrows for the Carousel

--- frontend/src/components/StartView/LeaseDisplayBox.vue
+++ frontend/src/components/StartView/LeaseDisplayBox.vue
@@ -10,6 +10,18 @@
         <div class="pricePerNightDiv"><b>Price Per Night:</b> {{ Math.round(lease.price * 1.15) }}</div>
       </div>
     </router-link>
+  <div @click="previousPicture" class="leftArrowBox" :id="lease.id">
+    <div @click="previousPicture" class="innerLeftArrowBox" :id="lease.id">
+      <i @click="previousPicture" class="arrow leftUpper" :id="lease.id"></i>
+      <i @click="previousPicture" class="arrow leftBottom" :id="lease.id"></i>
+    </div>
+  </div>
+  <div @click="nextPicture" class="rightArrowBox" :id="lease.id">
+    <div @click="nextPicture" class="innerRightArrowBox" :id="lease.id">
+      <i @click="nextPicture" class="arrow rightUpper" :id="lease.id"></i>
+      <i @click="nextPicture" class="arrow rightBottom" :id="lease.id"></i>
+    </div>
+  </div>
   </div>
 </template>
 <script setup="">
@@ -23,8 +35,17 @@ export default {
     }
   },
   methods: {
-    select: function(event) {
-      console.log(event.target);
+    previousPicture(event){
+      let oldImage = this.lease.imageURLs[0];
+      let newImage = this.lease.imageURLs[1];
+      this.lease.imageURLs[0] = newImage;
+      this.lease.imageURLs[1] = oldImage;
+    },
+    nextPicture(event){
+      let oldImage = this.lease.imageURLs[0];
+      let newImage = this.lease.imageURLs[1];
+      this.lease.imageURLs[0] = newImage;
+      this.lease.imageURLs[1] = oldImage;
     }
   }
 }
@@ -45,11 +66,59 @@ a:visited{
 .leaseBox{
   background-color:rgba(218, 224, 224, 0.8);
   min-width:max-content;
-  width:28vw;
+  min-width:280px;
   padding: 10px;
   text-align:center;
   margin: 5px;
   display:inline-block;
+  height:340px;
+}
+.leftArrowBox{
+  display:inline-block;
+  position:relative;
+  top:-180px;
+  left:-90px;
+  height:35px;
+  width:35px;
+  background-color:lightblue;
+  z-index:5;
+  transform:rotate(270deg);
+  border-radius:25px;
+  padding-top:5px;
+}
+
+.rightArrowBox{
+  display:inline-block;
+  position:relative;
+  top:-180px;
+  left:90px;
+  height:35px;
+  width:35px;
+  background-color:lightblue;
+  border-radius:25px;
+  z-index:5;
+  transform:rotate(90deg);
+  padding-top:5px;
+}
+
+.arrow {
+  border: solid black;
+  border-width: 6.1px 6.1px 6.1px 0;
+  display: inline-block;
+}
+.leftUpper, .rightUpper {
+  transform: rotate(45deg);
+  -webkit-transform: rotate(45deg);
+  position:relative;
+  left:0px;
+  top:5px;
+}
+.leftBottom, .rightBottom {
+  transform: rotate(135deg);
+  -webkit-transform: rotate(135deg);
+  position:relative;
+  left:0px;
+  top:5px;
 }
 
 @media only screen and (max-width: 400px) {
